7a81c8cf5a721f55cb9f09167aaaa93a585a48c2
tangjialiang <phx108@sina.com>
2016-11-25 10:49:56 +0800

    for test

--- src/entity/User.java
+++ src/entity/User.java
@@ -1,5 +1,32 @@
 package entity;
 
 public class User {
-
+	private String name ;
+	private String pwd ;
+	
+	public String getName() {
+		return name;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", pwd=" + pwd + "]";
+	}
+	public User(String name, String pwd) {
+		super();
+		this.name = name;
+		this.pwd = pwd;
+	}
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 }
