930298c885e0e2d99f621fa8db8dff450391898d
tangjialiang <phx108@sina.com>
2016-11-25 17:12:46 +0800

    continue to complete

--- src/bupt/tjlcast/framework/ActionServlet.java
+++ src/bupt/tjlcast/framework/ActionServlet.java
@@ -1,15 +1,17 @@
 package bupt.tjlcast.framework;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import bupt.tjlcast.framework.bean.ActionMapping;
 import bupt.tjlcast.framework.bean.ActionMappingManager;
+import bupt.tjlcast.framework.bean.Result;
 
 /**
  * Servlet implementation class ActionServlet
@@ -17,7 +19,7 @@ import bupt.tjlcast.framework.bean.ActionMappingManager;
  * 1. 拦截所有的*.action为后缀的请求
  * 2. 请求: http://localhost:8080/mystruts/login.action
  */
-@WebServlet("*.action")
+@WebServlet(name="AnnotationServlet",urlPatterns="*.action", loadOnStartup=1)
 public class ActionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private ActionMappingManager actionMappingManager = null ;
@@ -40,19 +42,40 @@ public class ActionServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// 1. 得到请求路径uri［login］
-		// 2. 根据路径名称，读取配置文件，得到类的全名 [bupt.....loginAction]
-		// 当前请求的处理方法
-		// 3. 通过反射：创建对象，调用方法，获取方法返回的标记
-		// 4. 拿到标记，读取配置文件得到标记对应的路径
-		// 5. 跳转
+		try {
+			// 1. 得到请求路径uri［/MyMVC/*.action］
+			String uri = request.getRequestURI() ;
+			// 得到action的请求路径 ［login］
+			String actionName = uri.substring(uri.lastIndexOf("/")+1, uri.indexOf(".action")) ;
+			
+			// 2. 根据路径名称，读取配置文件，得到类的全名 [bupt.....loginAction]
+			ActionMapping actionMapping = actionMappingManager.getActionMapping(actionName) ;
+			String className = actionMapping.getClassName() ;
+			// 当前请求的处理方法
+			String method = actionMapping.getMethod() ;
+			// 3. 通过反射：创建对象，调用方法，获取方法返回的标记
+			Class<?> clazz = Class.forName(className) ;
+			// 4. 拿到标记，读取配置文件得到标记对应的路径
+			Object obj = clazz.newInstance() ;
+			Method declaredMethod = clazz.getDeclaredMethod(method, HttpServletRequest.class, HttpServletResponse.class) ;
+			String returnFlag = (String) declaredMethod.invoke(obj, request, response) ;
+			
+			// 5. 跳转
+			Result result = actionMapping.getResults().get(returnFlag) ;
+			if ("redirect".equals(result.getPage())) {
+				response.sendRedirect(result.getPage());
+			} else {
+				request.getRequestDispatcher(result.getPage()).forward(request, response);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
